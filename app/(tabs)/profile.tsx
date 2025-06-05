da9fde292e3c418dbe473d16120d5d73dc7237d6
Klowi <antlover.me@gmail.com>
2025-06-05 12:36:09 +0800

    first commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Profile = () => {
+  return (
+    <View className="flex justify-center items-center mt-[50px] h-screen ">
+      <Text className="text-[50px] text-center text-blue-600 font-bold">
+        Profile
+      </Text>
+    </View>
+  );
+};
+
+export default Profile;
